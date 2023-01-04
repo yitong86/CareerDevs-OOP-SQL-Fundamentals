2bf77082f9286fc99f0de1aa16292afdcee1aef6
yitong86 <sophiajiang86@hotmail.com>
2023-01-04 14:05:41 -0500

    encapsulation

---
+++ src/encapsulation/Main.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.setName("Suzie");
+        s.setAge(4);
+        System.out.println(s.getName());
+
+    }
+}
---
+++ src/encapsulation/Student.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class Student {
+    private String name;
+    int age;
+    public void setName(String newName){
+        name = newName;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int newAge){
+        age = newAge;
+    }
+    public int getAge(){
+        return age;
+    }
+}
